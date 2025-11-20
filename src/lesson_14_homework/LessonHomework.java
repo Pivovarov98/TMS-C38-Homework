32ff658f359b7a57149e1ed89d68f8ada07d7390
Pivovarov98 <legion_19981@mail.ru>
2025-11-20 21:40:23 +0300

    feat: code correction

--- src/lesson_14_homework/LessonHomework.java
+++ src/lesson_14_homework/LessonHomework.java
@@ -4,14 +4,14 @@ import java.io.*;
 
 public class LessonHomework {
     public static void main(String[] args) throws IOException {
-        findLongestWordInFile("C:\\Users\\Admin\\IdeaProjects\\TMS-C38-Homework\\src\\lesson_14_homework\\romeo&juliet.txt");
+        findLongestWordInFile("src/lesson_14_homework/romeo&juliet.txt");
     }
 
     //Task 1
     static void findLongestWordInFile(String path) throws IOException {
         String longestWord = "";
         String word = "";
-        String outputFilePath = "C:\\Users\\Admin\\IdeaProjects\\TMS-C38-Homework\\src\\lesson_14_homework\\longest_word.txt";
+        String outputFilePath = "src/lesson_14_homework/longest_word.txt";
 
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             int charLetter;
