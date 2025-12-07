0da7aec1b37acea0576e685c325633cd1279ca02
Pivovarov98 <legion_19981@mail.ru>
2025-12-07 14:47:54 +0300

    feat: add solution to tasks 1

---
+++ src/lesson_18_homework/LessonHomework.java
@@ -0,0 +1,23 @@
+package lesson_18_homework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class LessonHomework {
+    public static void main(String[] args) {
+
+        //Task 1
+        streamsFunctions(new ArrayList<>(Arrays.asList(1, 2, 3, 1, 5, 7, 7, 5, 4, 4, 4, 1, 6)));
+        streamsFunctions(new ArrayList<>(Arrays.asList(1, 3, 5, 5, 6, 6, 1, 7, 6)));
+        streamsFunctions(new ArrayList<>(Arrays.asList(1, 3, 3, 5)));
+    }
+
+    static void streamsFunctions(ArrayList<Integer> numbers){
+        System.out.println(numbers.stream().
+                distinct().
+                filter(n -> n % 2 == 0).
+                mapToInt(Integer::intValue).
+                sum());
+    }
+}
