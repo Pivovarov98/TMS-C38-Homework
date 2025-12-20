1ba3d5ac6ca4c5689405cb5cdbbc42f9439d8b01
Pivovarov98 <legion_19981@mail.ru>
2025-12-20 20:26:42 +0300

    feat: add solution to tasks 3, 4, 5

--- src/lesson_17_homework/LessonHomework.java
+++ src/lesson_17_homework/LessonHomework.java
@@ -4,7 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class LessonHomework {
     public static void main(String[] args) {
@@ -14,6 +18,14 @@ public class LessonHomework {
 
         printPositiveNumbers(new ArrayList<>(Arrays.asList(1, 3, -6, 3, -3, -5, 6)));
         printPositiveNumbers(new ArrayList<>(Arrays.asList(-3, -4, -6, -1)));
+
+        bynToUsdWithFunction("30 BYN");
+        bynToUsdWithFunction("50 BYN");
+
+        bynToUsdWithConsumer("13 BYN");
+        bynToUsdWithConsumer("27 BYN");
+
+        reverseStringFromConsole();
     }
 
     //Task 1
@@ -29,5 +41,44 @@ public class LessonHomework {
         Predicate<Integer> isPositive = n -> n > 0;
         numbers.removeIf(num -> !isPositive.test(num));
         System.out.println(numbers.isEmpty() ? "There no positive numbers in that array" : numbers);
+    }
+
+    //Task 3
+    static void bynToUsdWithFunction(String amount){
+        double rate = 3.0;
+
+        Function<String, Double> bynToUsd = str -> {
+            String amountStr = str.split(" ")[0];
+            double byn = Double.parseDouble(amountStr);
+            return byn / rate;
+        };
+
+        System.out.println(bynToUsd.apply(amount) + " USD");
+    }
+
+    //Task 4
+    static void bynToUsdWithConsumer(String amount){
+        double rate = 3.0;
+
+        Consumer<String> bynToUsd = str -> {
+            String amountStr = str.split(" ")[0];
+            double byn = Double.parseDouble(amountStr);
+            double usd = byn / rate;
+            System.out.println(usd + " USD");
+        };
+
+        bynToUsd.accept(amount);
+    }
+
+    //Task 5
+    static void reverseStringFromConsole(){
+        Supplier<String> reverseString = () -> {
+            Scanner s = new Scanner(System.in);
+            System.out.print("Enter a string: ");
+            String i = s.nextLine();
+            return new StringBuilder(i).reverse().toString();
+        };
+
+        System.out.println("Reverse string: " + reverseString.get());
     }
 }
